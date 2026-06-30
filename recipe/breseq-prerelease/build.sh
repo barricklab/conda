fbe864b253e167fef687c18b39f1b6b8cbf042d6
Jeffrey Barrick <jeffrey.e.barrick@gmail.com>
2026-06-29 20:58:47 -0400

    Update to try to get linking working on aarch64

--- recipe/breseq-prerelease/build.sh
+++ recipe/breseq-prerelease/build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+[[ "$(uname)" == "Linux" ]] && export LDFLAGS="${LDFLAGS} -ldl"
+
+export INCLUDES="-I${PREFIX}/include"
+export LIBPATH="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export M4="${BUILD_PREFIX}/bin/m4"
+
 ./bootstrap.sh
-./configure --prefix="${PREFIX}"
+./configure --prefix="${PREFIX}" \
+	CXX="${CXX}" \
+	CXXFLAGS="${CXXFLAGS} -O3 -I${PREFIX}/include" \
+	LDFLAGS="${LDFLAGS}"
 make -j"${CPU_COUNT}"
 make install
