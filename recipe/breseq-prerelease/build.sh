7da1501c0c592d04aedbf02c3c30e49368263460
Jeffrey Barrick <jeffrey.e.barrick@gmail.com>
2026-06-29 21:35:44 -0400

    cleanup for libdl fix in breseq codebase

--- recipe/breseq-prerelease/build.sh
+++ recipe/breseq-prerelease/build.sh
@@ -2,17 +2,6 @@
 set -euo pipefail
 
 ./bootstrap.sh
-
-if [[ "$(uname)" == "Linux" ]]; then
-    ./configure \
-        --prefix=$PREFIX \
-        --with-pic \
-        LIBS="-ldl"
-else
-    ./configure \
-        --prefix=$PREFIX \
-        --with-pic
-fi
-	
+./configure --prefix="${PREFIX}"
 make -j"${CPU_COUNT}"
 make install
